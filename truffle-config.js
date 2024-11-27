ce3ab512c4cff3a00e4fb19bac64f2d0c7a2099d
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-28 03:32:24 +0900

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1"  // Use host.docker.internal for Docker
+      host: "127.0.0.1",  // Use host.docker.internal for Docker
       port: 7545,                    // Ganache's default port
       network_id: "*",               // Match any network id
     },
