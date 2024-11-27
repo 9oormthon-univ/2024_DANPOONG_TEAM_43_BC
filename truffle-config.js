8a4cda899d73c981d84c8afbf72a9e46604cc8e3
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-28 03:42:58 +0900

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,15 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",  // Use host.docker.internal for Docker
-      port: 7545,                    // Ganache's default port
-      network_id: "*",               // Match any network id
+      host: "127.0.0.1",  // Use localhost IP address
+      port: 7545,          // Ganache's default port
+      network_id: "*",     // Match any network id
+      networkCheckTimeout: 30000,  // Set a longer timeout (30 seconds)
     },
   },
   compilers: {
     solc: {
-      version: "0.8.21",  // Ensure your Solidity version matches the pragma in your contracts
+      version: "0.8.21",  // Ensure the Solidity version matches your contracts
     },
   },
 };
