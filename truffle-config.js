369f441a6ee9ceb6e47be468eccf7c831b97aacb
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-28 18:38:06 +0900

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  contracts_directory: "./contracts", // Solidity 파일 위치
-  contracts_build_directory: "./build/contracts", // 컴파일된 파일 저장 경로
+  contracts_directory: "./contracts",
+  contracts_build_directory: "./build/contracts",
   networks: {
     development: {
-      host: "127.0.0.1",  // Ganache 컨테이너와 연결
-      port: 7545,         // Ganache 기본 포트
-      network_id: "*",    // 모든 네트워크와 호환
-      networkCheckTimeout: 30000, // 네트워크 체크 타임아웃 설정
+      host: "ganache", // Docker Compose의 서비스 이름
+      port: 7545,      // Ganache 컨테이너의 포트
+      network_id: "*", // 모든 네트워크와 호환
     },
   },
   compilers: {
     solc: {
-      version: "0.8.20", // OpenZeppelin과 호환되는 Solidity 버전
+      version: "0.8.20", // Solidity 버전
     },
   },
 };
